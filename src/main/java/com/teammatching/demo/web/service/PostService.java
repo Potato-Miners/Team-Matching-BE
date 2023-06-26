aa1b426c02f9905c148b9835a3cee4e907ec0c71
Luke_YS <yjsmk0902@gmail.com>
2023-06-26 13:24:48 +0900

    #6 - PostController / PostService 생성
    * 게시글 엔티티 API 엔드포인트 설계에 따른 PostController 작성
    * 각 엔드포인트에서 사용할 비즈니스 로직을 담을 PostService 껍데기 추가

---
+++ src/main/java/com/teammatching/demo/web/service/PostService.java
@@ -0,0 +1,25 @@
+package com.teammatching.demo.web.service;
+
+import com.teammatching.demo.domain.dto.PostDto;
+import com.teammatching.demo.domain.dto.UserAccountDto;
+import org.springframework.data.domain.Page;
+
+public class PostService {
+    public Page<PostDto.SimpleResponse> getSimplePosts() {
+        return null;        //TODO: 비즈니스 로직 구현 필요
+    }
+
+    public void createPost(PostDto toDto) {
+        //TODO: 비즈니스 로직 구현 필요
+    }
+
+    public PostDto getPostById(Long postId) {
+        return null;        //TODO: 비즈니스 로직 구현 필요
+    }
+
+    public void updatePost(Long postId, PostDto toDto) {
+    }
+
+    public void deletePost(Long postId, UserAccountDto build) {
+    }
+}
