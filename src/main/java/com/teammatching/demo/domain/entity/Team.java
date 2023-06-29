3c7d4c0a23f2756392864331784935451b7841e6
Luke_YS <yjsmk0902@gmail.com>
2023-06-29 18:31:05 +0900

    #6 - Team 엔티티 및 DTO 수정 + Category 타입 추가
    * 업데이트된 ERD에 따라 Team 엔티티가 다음과 같이 수정됨 
    - 카테고리(category) 프로퍼티 추가
    - 팀 정원 (max -> limit) 으로 수정됨
    - 현재 인원 (total) 프로퍼티 추가
    - 마감일 (deadline) 프로퍼티 추가
    
    * Team 엔티티 수정에 따라 DTO 내용도  일부 수정 및 추가됨
    
    * 카테고리 프로퍼티 추가에 따른 Category 타입을 추가함

--- src/main/java/com/teammatching/demo/domain/entity/Team.java
+++ src/main/java/com/teammatching/demo/domain/entity/Team.java
@@ -1,9 +1,11 @@
 package com.teammatching.demo.domain.entity;
 
 import com.teammatching.demo.domain.AuditingField;
+import com.teammatching.demo.domain.Category;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -35,17 +37,38 @@ public class Team extends AuditingField {
     private String description;
 
     @Setter
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Setter
     @Column(nullable = false)
     private String hashtag;
+
+    @Setter
+    @Column(nullable = false)
+    private Integer limit;
+
+    @Setter
+    @Column(nullable = false)
+    private Integer total;
+
+    @Setter
+    private LocalDateTime deadline;
+
 
     @ToString.Exclude
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
     private final Set<Admission> admissions = new LinkedHashSet<>();
 
     @Builder
-    private Team(String name, String description, String hashtag, Long max) {
+    private Team(String adminId, String name, String description, Category category, String hashtag, Integer limit, Integer total, LocalDateTime deadline) {
+        this.adminId = adminId;
         this.name = name;
         this.description = description;
+        this.category = category;
         this.hashtag = hashtag;
+        this.limit = limit;
+        this.total = total;
+        this.deadline = deadline;
     }
 }
