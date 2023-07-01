582d76533bab6534fa4bc128e78318d0dfee56d0
Luke_YS <yjsmk0902@gmail.com>
2023-07-01 17:02:17 +0900

    #7 - 비즈니스 로직 일부 구현
    * CommentService / PostService / TeamService 구현
    * Update 부분 인증 정보 비교 부분 수정이 필요할 수 도 있음

--- src/main/java/com/teammatching/demo/web/service/TeamService.java
+++ src/main/java/com/teammatching/demo/web/service/TeamService.java
@@ -1,25 +1,62 @@
 package com.teammatching.demo.web.service;
 
 import com.teammatching.demo.domain.dto.TeamDto;
-import com.teammatching.demo.domain.dto.UserAccountDto;
+import com.teammatching.demo.domain.entity.Team;
+import com.teammatching.demo.domain.entity.UserAccount;
+import com.teammatching.demo.domain.repository.TeamRepository;
+import com.teammatching.demo.domain.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class TeamService {
-    public TeamDto getSimpleTeams() {
-        return null;
+
+    private final TeamRepository teamRepository;
+    private final UserAccountRepository userAccountRepository;
+
+    public Page<TeamDto> getSimpleTeams(Pageable pageable) {
+        return teamRepository.findAll(pageable).map(TeamDto::from);
     }
 
-    public void createTeam(TeamDto toDto, UserAccountDto build) {
+    public void createTeam(TeamDto request, String userId) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(userId);
+        teamRepository.save(request.toEntity(userAccount));
     }
 
     public TeamDto getTeamById(Long teamId) {
-        return null;
+        return teamRepository.findById(teamId)
+                .map(TeamDto::from)
+                .orElseThrow(RuntimeException::new);    //TODO: 예외 처리 구현 필요
     }
 
-    public void updateTeam(Long teamId, TeamDto toDto, UserAccountDto build) {
+    public void updateTeam(Long teamId, TeamDto request, String userId) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(userId);
+        UserAccount requestAccount = userAccountRepository.getReferenceById(request.adminId());
+        Team team = teamRepository.findById(teamId).orElseThrow(RuntimeException::new);     //TODO: 예외 처리 구현 필요
+        if (requestAccount.equals(userAccount)) {
+            if (request.name() != null) {
+                team.setName(request.name());
+            }
+            if (request.category() != null) {
+                team.setCategory(request.category());
+            }
+            if (request.hashtag() != null) {
+                team.setHashtag(request.hashtag());
+            }
+            if (request.total() != null) {
+                team.setTotal(request.total());
+            }
+            team.setDescription(request.description());
+            team.setLimit(request.limit());
+        }
     }
 
-    public void deleteTeam(Long teamId, UserAccountDto build) {
+    public void deleteTeam(Long teamId, String userId) {
+        teamRepository.deleteByIdAndAdminId(teamId, userId);
     }
 }
