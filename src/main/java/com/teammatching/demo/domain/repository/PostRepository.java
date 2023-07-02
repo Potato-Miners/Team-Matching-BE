bc12641829b870e894abb90b242ff4c6cfd17e5b
Luke_YS <yjsmk0902@gmail.com>
2023-07-02 16:35:55 +0900

    #7 - 일부 Repository 수정
    * MyPageController 구현을 위해 각 Comment / Post / Team 각 Repository에 유저 PK로 해당 엔티티를 검색할 수 있도록 쿼리메소드를 추가함

--- src/main/java/com/teammatching/demo/domain/repository/PostRepository.java
+++ src/main/java/com/teammatching/demo/domain/repository/PostRepository.java
@@ -1,8 +1,13 @@
 package com.teammatching.demo.domain.repository;
 
 import com.teammatching.demo.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByUserAccount_UserId(String userId, Pageable pageable);
+
     void deleteByIdAndUserAccount_UserId(Long postId, String userId);
 }
