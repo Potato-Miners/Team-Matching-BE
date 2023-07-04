3f95dbf12b9f6f5c891e842652c00f5b8b17073d
Luke_YS <yjsmk0902@gmail.com>
2023-07-04 11:26:31 +0900

    #7 - AdmissionRepository 수정
    * teamId를 찾아 Admission Page를 반환하는 쿼리메소드 추가
    * 가입 신청 현황을 볼 수 있는 쿼리 메소드 추가
    * 가입 신청이 되어 있는지 확인하는 쿼리 메소드 추가

--- src/main/java/com/teammatching/demo/domain/repository/AdmissionRepository.java
+++ src/main/java/com/teammatching/demo/domain/repository/AdmissionRepository.java
@@ -1,7 +1,18 @@
 package com.teammatching.demo.domain.repository;
 
 import com.teammatching.demo.domain.entity.Admission;
+import com.teammatching.demo.domain.entity.Team;
+import com.teammatching.demo.domain.entity.UserAccount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdmissionRepository extends JpaRepository<Admission, Long> {
+
+    Page<Admission> findByTeam_Id(Long teamId, Pageable pageable);
+
+    Admission findByUserAccount_UserId(String userId);
+
+    Boolean findByUserAccountAndTeam(UserAccount userAccount, Team team);
+
 }
