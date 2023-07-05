8ffb4ecaec2ecf53f971747b80db91462cb800e5
Luke_YS <yjsmk0902@gmail.com>
2023-07-05 15:12:49 +0900

    #8 - Admission Repo / Service 수정
    * AdmissionRespository : findByUserAccountAndTeam 쿼리메소드의 반환형을 Boolean에서 Optional<Admission>으로 변경
    (ifPresent 메소드를 사용하기 위함)
    * 해당 변경사항을 AdmissionService에서도 적용

--- src/main/java/com/teammatching/demo/domain/repository/AdmissionRepository.java
+++ src/main/java/com/teammatching/demo/domain/repository/AdmissionRepository.java
@@ -7,12 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdmissionRepository extends JpaRepository<Admission, Long> {
 
     Page<Admission> findByTeam_Id(Long teamId, Pageable pageable);
 
     Admission findByUserAccount_UserId(String userId);
 
-    Boolean findByUserAccountAndTeam(UserAccount userAccount, Team team);
+    Optional<Admission> findByUserAccountAndTeam(UserAccount userAccount, Team team);
 
 }
