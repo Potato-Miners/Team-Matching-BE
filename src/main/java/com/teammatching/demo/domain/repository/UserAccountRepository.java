f97ce0c9277eed94a1ecbf6ee6f9aa41f3efb817
Luke_YS <yjsmk0902@gmail.com>
2023-07-05 15:14:42 +0900

    #8 - UserAccountRepository 수정
    * 회원가입시 가입 중복 체크를 위한 3가지 find 쿼리 메소드 생성

--- src/main/java/com/teammatching/demo/domain/repository/UserAccountRepository.java
+++ src/main/java/com/teammatching/demo/domain/repository/UserAccountRepository.java
@@ -3,6 +3,14 @@ package com.teammatching.demo.domain.repository;
 import com.teammatching.demo.domain.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+
+    Optional<UserAccount> findByEmail(String email);
+
+    Optional<UserAccount> findByNickname(String nickname);
+
+    Optional<UserAccount> findByRefreshToken(String refreshToken);
 
 }
