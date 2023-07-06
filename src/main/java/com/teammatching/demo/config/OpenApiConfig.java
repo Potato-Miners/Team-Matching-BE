9167f43061ef8fe56a6e342329bb9ba0ee800b4d
Luke_YS <yjsmk0902@gmail.com>
2023-07-06 10:54:01 +0900

    #23 - OpenApiConfig 수정
    * SwaggerUI 그룹핑 설정 - default
        - springSecurityLoginEndPointCustomiser를 통해 로그인 엔드포인트에 대한 식별과 설명 추가
        - 해당 추가된 customiser를 추가함

--- src/main/java/com/teammatching/demo/config/OpenApiConfig.java
+++ src/main/java/com/teammatching/demo/config/OpenApiConfig.java
@@ -1,14 +1,48 @@
 package com.teammatching.demo.config;
 
 
+import com.teammatching.demo.web.filter.login.CustomJsonUsernamePasswordAuthenticationFilter;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Configuration
 public class OpenApiConfig {
+
+    private final ApplicationContext applicationContext;
+
+    private static final String DEFAULT_LOGIN_REQUEST_URL = "/login";
+    private static final String USERNAME_KEY = "userId";
+    private static final String PASSWORD_KEY = "userPassword";
+
+    @Bean
+    public GroupedOpenApi defaultAPI() {
+        return GroupedOpenApi.builder()
+                .group("default")
+                .pathsToMatch("/**")
+                .addOpenApiCustomiser(springSecurityLoginEndpointCustomiser())
+                .build();
+    }
+
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
@@ -18,5 +52,43 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .components(new Components())
                 .info(info);
+    }
+
+    @Bean
+    public OpenApiCustomiser springSecurityLoginEndpointCustomiser() {
+        FilterChainProxy filterChainProxy = applicationContext.getBean(
+                AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME,
+                FilterChainProxy.class);
+        return openAPI -> {
+            for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
+                Optional<CustomJsonUsernamePasswordAuthenticationFilter> optionalFilter =
+                        filterChain.getFilters().stream()
+                                .filter(CustomJsonUsernamePasswordAuthenticationFilter.class::isInstance)
+                                .map(CustomJsonUsernamePasswordAuthenticationFilter.class::cast)
+                                .findAny();
+                if (optionalFilter.isPresent()) {
+                    CustomJsonUsernamePasswordAuthenticationFilter filter = optionalFilter.get();
+                    Operation operation = new Operation();
+                    Schema<?> schema = new ObjectSchema()
+                            .addProperties(USERNAME_KEY, new StringSchema())
+                            .addProperties(PASSWORD_KEY, new StringSchema());
+                    RequestBody requestBody = new RequestBody().content(
+                            new Content().addMediaType(
+                                    org.springframework.http.MediaType.APPLICATION_JSON_VALUE,
+                                    new MediaType().schema(schema)));
+                    operation.requestBody(requestBody);
+                    ApiResponses apiResponses = new ApiResponses();
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.OK.value()),
+                            new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                    apiResponses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                            new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase()));
+                    operation.responses(apiResponses);
+                    operation.addTagsItem("인증 API");
+                    operation.summary("로그인").description("아이디와 비밀번호를 받아 로그인합니다.");
+                    PathItem pathItem = new PathItem().post(operation);
+                    openAPI.getPaths().addPathItem(DEFAULT_LOGIN_REQUEST_URL, pathItem);
+                }
+            }
+        };
     }
 }
