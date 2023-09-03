3c1c5d07c3f484f561bab5927b6ac9823c40b8e4
Luke_YS <yjsmk0902@gmail.com>
2023-09-03 23:58:25 +0900

    main
    - prod 서버 타임존 설정

--- src/main/java/com/teammatching/demo/TeamMatchingBeApplication.java
+++ src/main/java/com/teammatching/demo/TeamMatchingBeApplication.java
@@ -3,6 +3,9 @@ package com.teammatching.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TeamMatchingBeApplication {
 
@@ -10,4 +13,8 @@ public class TeamMatchingBeApplication {
 		SpringApplication.run(TeamMatchingBeApplication.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
