9194dac6d966f575b7e1def25cc9c4a05620be7c
Luke_YS <yjsmk0902@gmail.com>
2024-04-13 09:32:43 +0900

    :bug: Fix: SecurityConfig 수정
    - CORS 설정 추가
    - /favicon.ico 경로 인증 해제

--- src/main/java/com/teammatching/demo/global/security/SecurityConfig.java
+++ src/main/java/com/teammatching/demo/global/security/SecurityConfig.java
@@ -23,6 +23,12 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -37,10 +43,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .formLogin().disable()
                 .httpBasic().disable()
                 .csrf().disable()
+                .cors(configurer -> configurer.configurationSource(apiConfigurationSource()))
                 .headers().frameOptions().disable()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -48,7 +55,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .antMatchers(
-                                "/sign-up", "/login", "/logout",
+                                "/sign-up", "/login", "/logout", "/favicon.ico",
                                 "/swagger-ui.html", "/v1/api-docs/**", "/swagger-ui/**",
                                 "/api/r/**"
                         ).permitAll()
@@ -74,7 +81,7 @@ public class SecurityConfig {
 
     @Bean
     public LoginSuccessHandler loginSuccessHandler() {
-        return new LoginSuccessHandler(jwtService, userAccountRepository,objectMapper);
+        return new LoginSuccessHandler(jwtService, userAccountRepository, objectMapper);
     }
 
     @Bean
@@ -95,5 +102,19 @@ public class SecurityConfig {
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
         return new JwtAuthenticationProcessingFilter(jwtService, userAccountRepository, redisTemplate);
+    }
+
+    CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
